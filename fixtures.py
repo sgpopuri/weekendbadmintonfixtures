61e4e77859845881fac600233edbdad65f8990b9
popuri <sgpopuri@gmail.com>
2020-10-11 10:58:20 +0530

    Update and rename fixtures to fixtures.py
    comments

--- fixtures.py
+++ fixtures.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from itertools import combinations
 import random
+
+#accepts a csv file with two columns: "Name" and "Rating". making the csv is outside the scope, but a google form can easily generate one
 dat = pd.read_csv('form_data.csv')
 
 courts = 2
@@ -13,13 +15,16 @@ players_rate = list(dat[["Name","Rating"]].to_records(index=False))
 players=[x for (x,y) in players_rate]
 dtn_cnt = dict((x,0) for x in players)
 # dtn=dict((x,y) for (x,y) in players_rate)
+# or below
 dtn={}
 for (x,y) in players_rate:
         dtn.setdefault(x,y)
 
 players_per_round = courts * 4
 # rounds_per_player = (rounds * players_per_round)/len(players)
-
+# the below picks up players_per_round players from the full list of players.
+# This is a fair pickup meaning no player will sit out morethan one round unless we have more than double the players than courts.
+# This gives a good balance if the players are a little more than required per court. For example 5-6 players for each court.
 all_rounds = list(combinations(players,players_per_round))
 ll=[]
 ll.append(all_rounds[0])
@@ -28,7 +33,10 @@ for i in range(len(all_rounds)):
         [x for x in all_rounds if set(set(players).difference(set(ll[-1]))).isdisjoint(set(players).difference(set(x))) and (ll.append(x) or all_rounds.remove(x))]
 if all_rounds:
         ll.extend(all_rounds)
-        
+
+# this will generate all possible pairs with each player getting a chance to play for each player.
+# the logic makes sure once a player is chosen for a court, he is not in any other court in the same round.
+# AI: could be simplified
 full_fixtures=[]
 for round in ll:
         r_pl_f=[x for x in players if x in round]
@@ -60,6 +68,7 @@ for round in ll:
                 full_fixtures.append(('--', '--', 0))
                 full_fixtures.append(('--', '--', 0))
 
+# Here pairs who didn't get chance will get chance if we can't balance above.
 part_fix=[(a,b) for (a,b,c) in full_fixtures if a !='--']
 full_pairs = list(combinations(players,2))
 diff_pairs=list(set(full_pairs).difference(set(part_fix)))
@@ -93,6 +102,7 @@ while dpr and len(dpr) > numpairs_round-1:
 it=iter(full_fixtures)
 final=list(zip(it,it))
 
+# Now output (i.e. the final fixtures) goes into another csv file "full_fixtures.csv"
 df=pd.DataFrame()
 # df = pd.DataFrame.from_records(final,columns=['Team1','Team2'])
 
@@ -108,6 +118,7 @@ df.insert(8,'count',pd.Series(['=COUNTIF(B2:F200,"*"&I'+str(i)+'&"*")' for i in 
 df.insert(9, 'players rating',pd.Series([b for (a,b) in players_rate]),True)
 df.to_csv('full_fixtures.csv')
 
+# test prints to check the fixtures
 print(len(full_fixtures))
 print(len(part_fix))
 print(len(diff_pairs))
